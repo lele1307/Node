930a9514ad3d1da538e660c7a1873656e213df3d
lele1307 <dkh1996@126.com>
2020-07-05 16:24:39 +0100

    Bar Style Set

--- Practice-d3/bar/index.js
+++ Practice-d3/bar/index.js
@@ -16,7 +16,7 @@ import {
     const render = data => {
         const xValue = d=>d.population;
         const yValue = d=>d.country;
-        const margin = {top:50,right:40,left:200,bottom:20};
+        const margin = {top:50,right:40,left:200,bottom:70};
         const innerWidth = width - margin.left - margin.right;
         const innerHeight = height - margin.top - margin.bottom;
 
@@ -36,24 +36,35 @@ import {
             .replace('G','B');
 
         const xAxis = axisBottom(xScale)
-            .tickFormat(xAxisTickFormat);
-        
-        g.append('g')
-            .call(axisLeft(yScale))
-            .selectAll('.domain,.tick line')
-                .remove();
+            .tickFormat(xAxisTickFormat)
+            .tickSize(-innerHeight+4);
 
-        g.append('g').call(xAxis)
-            .attr('transform',`translate(0,${innerHeight})`)
-            .selectAll('.domain')
-                .remove();
+        const xAxisG = g.append('g').call(xAxis).attr('transform',`translate(0,${innerHeight})`);
+        xAxisG.selectAll('.domain').remove();
+        xAxisG.append('text')
+            .attr('class','axis-label')
+            .attr('y',40)
+            .attr('x',innerWidth / 2)
+            .attr('fill','black')
+            .text('[Population]');
+
+        const yAxisG = g.append('g').call(axisLeft(yScale));
+        yAxisG.selectAll('.domain,.tick line').remove();
+        yAxisG.append('text')
+            .attr('class','axis-label')
+            .attr('y',innerHeight / 2)
+            .attr('x', -70)
+            .attr('fill','black')
+            .text('[Countries]');
 
         g.selectAll('rect').data(data)
             .enter().append('rect')
                 .attr('y',d => yScale(yValue(d)))
                 .attr('width',d=>xScale(xValue(d)))
                 .attr('height',yScale.bandwidth()); 
+
         g.append('text')
+            .attr('class','title')
             .attr('y',-10)
             .text('Top 10 Most Populous Countries');
     }
