0e15ebd4bad46d93266a23b104602291c93a510c
lele1307 <dkh1996@126.com>
2020-07-06 21:43:13 +0100

    add map color

--- Practice-d3/src/map/index.js
+++ Practice-d3/src/map/index.js
@@ -1,21 +1,24 @@
 import '../css/map.css';
 import { feature } from 'topojson';
-import { select,json,geoPath,geoMercator} from 'd3';
+import { select,json,geoPath,geoMercator,geoOrthographic,geoNaturalEarth1} from 'd3';
 
 const svg = select('svg');
-const width = +svg.attr('width');
-const height = +svg.attr('height');
 
-const projection = geoMercator();//球面墨卡托投影.
+//const projection = geoMercator();//球面墨卡托投影.
+//const projection = geoOrthographic();//方位角正投影.
+const projection = geoNaturalEarth1();
 const pathGenerator = geoPath().projection(projection);//创建一个新的地理路径生成器.
+svg.append('path')
+  .attr('class','sphere')
+  .attr('d',pathGenerator({type:'Sphere'}));
 
 json ('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
   .then(data=>{
     const countries = feature(data,data.objects.countries);
-    console.log(countries);
 
-    const paths = svg.selectAll('path')
-      .data(countries.features);
-    paths.enter().append('path')
-      .attr('d',d => pathGenerator(d));
+    console.log(countries);
+    svg.selectAll('path').data(countries.features)
+      .enter().append('path')
+      .attr('class','country')
+      .attr('d',pathGenerator);
   })
