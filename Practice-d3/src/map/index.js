8acee89afaf92b0716de54a0c6241f42846c40c1
lele1307 <dkh1996@126.com>
2020-07-06 22:21:33 +0100

    map interaction

--- Practice-d3/src/map/index.js
+++ Practice-d3/src/map/index.js
@@ -1,24 +1,41 @@
 import '../css/map.css';
 import { feature } from 'topojson';
-import { select,json,geoPath,geoMercator,geoOrthographic,geoNaturalEarth1} from 'd3';
+import { select,json,tsv,geoNaturalEarth1,geoPath,zoom,event,
+  geoMercator,geoOrthographic} from 'd3';
 
 const svg = select('svg');
-
 //const projection = geoMercator();//球面墨卡托投影.
 //const projection = geoOrthographic();//方位角正投影.
 const projection = geoNaturalEarth1();
 const pathGenerator = geoPath().projection(projection);//创建一个新的地理路径生成器.
-svg.append('path')
+const g = svg.append('g');
+g.append('path')
   .attr('class','sphere')
   .attr('d',pathGenerator({type:'Sphere'}));
 
-json ('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
-  .then(data=>{
-    const countries = feature(data,data.objects.countries);
+svg.call(zoom().on('zoom',()=>{
+  g.attr('transform',event.transform);
+}));
 
-    console.log(countries);
-    svg.selectAll('path').data(countries.features)
+Promise.all([
+  tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
+  json ('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
+]).then(([tsvData,topoJSONData]) => {
+  //console.log(tsvData);
+  /*const countryName = {};
+  tsvData.forEach(d => {
+      countryName[d.iso_n3] = d.name;
+  }); */
+  //console.log(topoJSONData);
+  const countryName = tsvData.reduce((accumulator,d)=>{
+    accumulator[d.iso_n3] = d.name;
+    return accumulator;
+  },{});
+  const countries = feature(topoJSONData,topoJSONData.objects.countries);
+    g.selectAll('path').data(countries.features)
       .enter().append('path')
-      .attr('class','country')
-      .attr('d',pathGenerator);
-  })
+        .attr('class','country')
+        .attr('d',pathGenerator)
+      .append('title')
+        .text(d => countryName[d.id]);
+});
